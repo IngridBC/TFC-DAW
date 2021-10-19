9a28c61184ce449b00c5d762437e6f1e4586c98e
IngridBC <91843597+IngridBC@users.noreply.github.com>
2021-10-19 15:50:49 +0200

    Delete partials directory

--- partials/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- FOOTER SECTION STARTS-->
-<div class="footer">
-        <div class="wrapper">
-        <label class="footer-info text-center" for=""> Bakery Co.&#169 2021. All rights reserved. </label>
-        </div>
-    </div>
-</body>
-</html>
