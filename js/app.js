9b1baeca4f705aa2713dcd28e51b6e5b6db98a76
IngridBC <91843597+IngridBC@users.noreply.github.com>
2021-12-06 22:10:11 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const btnMenu= document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+        
+btnMenu.addEventListener('click', ()=>{
+    btnMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
+
+const btns=document.querySelectorAll('.nav-btn');
+const slides= document.querySelectorAll('.video-slide');
+const contents= document.querySelectorAll('.content');
+
+var sliderNav=function(manual){
+    btns.forEach((btn)=>{
+    btn.classList.remove('active');
+    });
+
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+    });
+
+    contents.forEach((content)=>{
+        content.classList.remove('active');
+    });
+
+    btns[manual].classList.add('active');
+    slides[manual].classList.add('active');
+    contents[manual].classList.add('active');
+}
+
+btns.forEach((btn,i)=>{
+    btn.addEventListener('click', ()=>{
+        sliderNav(i);
+    });
+});
+
+/*-------------COUNTER--------- */
+
+app.use("/js", express.static('./js/'));
+
+jQuery(document).ready(function( $ ){
+    $('.time-counter').counterUp({
+        delay:10,
+        time:1200
+    });
+});
+
+
+/*-------------SLIDER CAROUSEL TESTIMONIES--------- */
+
+$(".slider").owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
